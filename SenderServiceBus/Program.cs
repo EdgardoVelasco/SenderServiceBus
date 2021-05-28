8ccf480e39e750eccf4979abdd8204c08ae91b2f
edgardo.nvf <edgardo.nvf@gmail.com>
2021-05-28 16:10:54 -0500

    Sender

---
+++ SenderServiceBus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Azure.Messaging.ServiceBus;
+using SenderServiceBus.bus;
+namespace SenderServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente = Busclient.Cliente;
+            Console.WriteLine("-.-.-.Sender de mensajes-.-.");
+            Console.WriteLine("mensaje a enviar: ");
+            var mensaje = Console.ReadLine();
+            SendMessage(cliente, mensaje);
+
+            Console.ReadLine();
+            
+        }
+
+        private static async void SendMessage(ServiceBusClient cliente, string mensaje) {
+            var sender = cliente.CreateSender("tienda");
+            await sender.SendMessageAsync(new ServiceBusMessage(mensaje));
+            Console.WriteLine("Se ha enviado el mensaje");
+        }
+    }
+}
---
+++ SenderServiceBus/bus/Busclient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+
+
+namespace SenderServiceBus.bus
+{
+    class Busclient
+    {
+        private static readonly string CONN_STRING = "Endpoint=sb://serviceenvf.servicebus.windows.net/;SharedAccessKeyName=Sender;SharedAccessKey=KwkaOMsG/5rNXEZTWyolBy38zgsllR5cvZPI3nQHfcY=";
+        
+
+        public static ServiceBusClient Cliente { get; private set; }
+
+        static Busclient() { InitBus(); }
+
+        private static void InitBus() {
+            if (Cliente==null) {
+                Cliente = new ServiceBusClient(CONN_STRING);
+            }
+        }
+
+
+    }
+}
